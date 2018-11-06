93f33a5fca49ddd4e850ed65fe0be51e2b99b998
Mervyn <404245127@qq.com>
2018-11-06 11:58:46 +0800

    roman to integer

---
+++ 013.roman-to-integer/roman_to_integer_test.go
@@ -0,0 +1,26 @@
+package p013
+
+import "testing"
+
+func TestIsPalindrome(t *testing.T) {
+	if romanToInt("III") != 3 {
+		t.Error("121 is palindrome")
+	}
+
+	if 	romanToInt("IV") != 4 {
+		t.Error("0 is palindrome")
+	}
+
+	if 	romanToInt("IX") != 9 {
+		t.Error("0 is palindrome")
+	}
+
+	if 	romanToInt("LVIII") != 58 {
+		t.Error("0 is palindrome")
+	}
+
+	if 	romanToInt("MCMXCIV") != 1994 {
+		t.Error("0 is palindrome")
+	}
+
+}
